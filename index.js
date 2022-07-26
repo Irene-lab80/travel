ffd394d39b3b41e80be94d3ad42f58c11e660079
Irina <irene7744@gmail.com>
2022-07-26 16:03:51 +0300

    refactor: slider btns

--- index.js
+++ index.js
@@ -1,43 +1,48 @@
 const pagination = document.querySelector(".slider__pagination");
 const dots = document.querySelectorAll(".slider__dot");
-
 const slider = document.querySelector(".slider__container");
-const slierItem = document.querySelectorAll(".slider__item");
+const sliderItems = document.querySelectorAll(".slider__item");
+let currentSlide;
 
-pagination.addEventListener("click", (e) => {
-  if (e.target.classList.contains("slider__dot")) {
-    if (e.target == document.getElementById("btn1")) {
-      slider.style.transform = "translate(860px)";
-    } else if (e.target == document.getElementById("btn2")) {
-      slider.style.transform = "translate(0)";
-    } else if (e.target == document.getElementById("btn3")) {
-      slider.style.transform = "translate(-860px)";
-    }
+// slider
+function slide(current) {
+  if (current === 1) {
+    slider.style.transform = "translate(860px)";
+  } else if (current === 2) {
+    slider.style.transform = "translate(0)";
+  } else if (current === 3) {
+    slider.style.transform = "translate(-860px)";
   }
+}
+
+function setCurrentSlide(current) {
+  currentSlide = current;
+  slide(current);
+}
+
+sliderItems.forEach((el, index) => {
+  el.addEventListener("click", () => {
+    setCurrentSlide(index + 1);
+    styleBtns();
+  });
 });
 
-slider.addEventListener("click", (e) => {
-  if (e.target.parentElement.classList.contains("slider__item")) {
-    if (e.target.parentElement == document.getElementById("slide1")) {
-      slider.style.transform = "translate(860px)";
-    } else if (e.target.parentElement == document.getElementById("slide2")) {
-      slider.style.transform = "translate(0)";
-    } else if (e.target.parentElement == document.getElementById("slide3")) {
-      slider.style.transform = "translate(-860px)";
-    }
-  }
+dots.forEach((el, index) => {
+  el.addEventListener("click", () => {
+    setCurrentSlide(index + 1);
+    styleBtns();
+  });
 });
 
-// active btns styles
-const setActiveButtonStyle = () => {
-  pagination.addEventListener("click", (e) => {
-    if (e.target.classList.contains("slider__dot")) {
-      dots.forEach((el) => {
-        el.classList.remove("active");
-      });
-      e.target.classList.add("active");
+function styleBtns() {
+  dots.forEach((el, index) => {
+    el.classList.remove("active");
+    if (index + 1 == currentSlide) {
+      el.classList.add("active");
     }
   });
-};
+}
 
-setActiveButtonStyle();
+window.addEventListener("load", () => {
+  setCurrentSlide(2);
+});
